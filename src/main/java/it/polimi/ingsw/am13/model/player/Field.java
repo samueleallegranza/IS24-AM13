ae87553c49d01ab30e58e6008b4be8eda15632ce
Lbattini <xbatxxx6t@gmail.com>
2024-03-21 18:41:08 +0100

    Updated partial Field code

--- src/main/java/it/polimi/ingsw/am13/model/player/Field.java
+++ src/main/java/it/polimi/ingsw/am13/model/player/Field.java
@@ -1,22 +1,31 @@
 package it.polimi.ingsw.am13.model.player;
 
-import it.polimi.ingsw.am13.model.card.Card;
-import it.polimi.ingsw.am13.model.card.CardStarter;
-import it.polimi.ingsw.am13.model.card.Coordinates;
+import it.polimi.ingsw.am13.model.card.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Field {
+public class Field {
 
-    private CardStarter startCard;
-    private Map<Coordinates, Card> field;
+    //private CardStarter startCard; //I'm pretty sure this is useless, you can just get the card at (0,0)
+    private Map<Coordinates, CardSidePlayable> field;
 
+    public Field() {
+        field=new HashMap<>();
+    }
+
+    public CardSidePlayable getCardAt(Coordinates coordinates){
+        return field.get(coordinates);
+    }
     public List<Coordinates> getAvailablePos() {
         return null;
     }
 
-    public void playCard(Card card, Coordinates coord) {
+    public void playCardSide(CardSidePlayable card, Coordinates coord) {
+        field.put(coord,card);
     }
+
+    //public void addCardSide(CardSidePlayable card, Coordinates coord) I believe that's what the above method does
 
 }
