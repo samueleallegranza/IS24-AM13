18dd728839619a073aeb78b7fc9ef3744a42d517
Matteo <matteo.arrigo@mail.polimi.it>
2024-03-23 10:00:32 +0100

    Completed class Coordinates, created TestCoordinates, InvalidCoordinatesException

---
+++ src/main/java/it/polimi/ingsw/am13/model/exceptions/InvalidCoordinatesException.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.am13.model.exceptions;
+
+/**
+ * Represents invalid game coordinates, that is coordinates whose sum is not a multiple of 2, or more generically
+ * game coordinates used improperly during the game itself.
+ */
+public class InvalidCoordinatesException extends ModelException {
+}
