a4332edfca0aac521fd48e09a9b9ca75ca472612
Samuele Allegranza <samuele.allegranza@gmail.com>
2024-05-27 19:04:56 +0200

    First implementation of Player Container + ViewGUIControllerMatch code restructure

--- src/main/java/it/polimi/ingsw/am13/client/view/gui/ViewGUIControllerMatch.java
+++ src/main/java/it/polimi/ingsw/am13/client/view/gui/ViewGUIControllerMatch.java
@@ -4,25 +4,31 @@ import it.polimi.ingsw.am13.client.gamestate.GameState;
 import it.polimi.ingsw.am13.controller.RoomIF;
 import it.polimi.ingsw.am13.model.card.*;
 import it.polimi.ingsw.am13.model.exceptions.RequirementsNotMetException;
+import it.polimi.ingsw.am13.model.player.Player;
 import it.polimi.ingsw.am13.model.player.PlayerLobby;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class ViewGUIControllerMatch extends ViewGUIController {
@@ -34,7 +40,8 @@ public class ViewGUIControllerMatch extends ViewGUIController {
     private Pane handCardsContainer;
 
     @FXML
-    private Pane playersContainer;
+    private GridPane playersContainer;
+//    private Map<String, Node> playerNodes; TODO: implement! ~Samu
 
     @FXML
     private Pane pickablesContainer;
@@ -68,6 +75,10 @@ public class ViewGUIControllerMatch extends ViewGUIController {
     private int playedCardIndex;
     private static final Integer imageW=150,imageH=100,cornerX=35,cornerY=40;
 
+    // ----------------------------------------------------------------
+    //      CONTROLLER METHODS
+    // ----------------------------------------------------------------
+
     @Override
     public void setPlayer(PlayerLobby player) {
         this.player=player;
@@ -76,26 +87,6 @@ public class ViewGUIControllerMatch extends ViewGUIController {
     @Override
     public void setGameState(GameState gameState) {
         this.state=gameState;
-    }
-
-    @Override
-    public void showStartupScreen(boolean isSocket, String ip, int port) {
-
-    }
-
-    @Override
-    public void showRooms(List<RoomIF> rooms) {
-
-    }
-
-    @Override
-    public void showPlayerJoinedRoom(PlayerLobby player) {
-
-    }
-
-    @Override
-    public void showStartGame(GameState state) {
-
     }
 
     @Override
@@ -149,115 +140,6 @@ public class ViewGUIControllerMatch extends ViewGUIController {
         }
     }
 
-    @Override
-    public void setGameId(int gameId) {
-
-    }
-
-    @Override
-    public void showPlayedStarter(PlayerLobby player) {
-
-    }
-
-    @Override
-    public void showChosenPersonalObjective(PlayerLobby player) {
-
-    }
-
-    private void displayCard(String id, Side side, ImageView imageView){
-        Image cardImage;
-        if(side==Side.SIDEBACK && id.charAt(0)!='s')
-            if (id.charAt(0)=='o')
-                id="o000";
-            else
-                id=id.substring(0,3)+'0';
-        if(side==Side.SIDEFRONT)
-            cardImage = new Image(getClass().getResourceAsStream("/img/cards/fronts/" + id + ".png"));
-        else
-            cardImage = new Image(getClass().getResourceAsStream("/img/cards/backs/" + id + ".png"));
-        imageView.setImage(cardImage);
-        imageView.setFitHeight(imageH);
-        imageView.setFitWidth(imageW);
-    }
-
-    private void makeDraggable(int handPlayableIndex, ImageView imageView){
-        imageView.setId(String.valueOf(handPlayableIndex));
-        imageView.setOnDragDetected(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent event) {
-                Dragboard db = imageView.startDragAndDrop(TransferMode.ANY);
-                ClipboardContent content = new ClipboardContent();
-                content.putString(imageView.getId());
-                db.setContent(content);
-                event.consume();
-            }
-        });
-        imageView.setOnDragDone(new EventHandler<DragEvent>() {
-            public void handle(DragEvent event) {
-                if (event.getTransferMode() == TransferMode.MOVE) {
-                    //imageView.setImage(null);
-                    attemptedToPlayCardHand=imageView;
-                    handCardsContainer.getChildren().remove(imageView);
-                    for (int i = 0; i < handCardsContainer.getChildren().size(); i++) {
-                        if(handCardsContainer.getChildren().get(i).getOnDragDetected()!=null)
-                            handCardsContainer.getChildren().get(i).setOnDragDetected(null);
-                    }
-                }
-                event.consume();
-            }
-        });
-    }
-
-    private void addCardBox(Coordinates coordinates, List<CardPlayableIF> finalHandPlayable){
-        Rectangle box1 = new Rectangle(imageW, imageH, Color.BLUE);
-        int posX=(imageW-cornerX)*coordinates.getPosX();
-        int posY=(-imageH+cornerY)*coordinates.getPosY();
-        box1.setTranslateX(posX);
-        box1.setTranslateY(posY);
-        box1.setOnDragOver(new EventHandler<DragEvent>() {
-            public void handle(DragEvent event) {
-                event.acceptTransferModes(TransferMode.MOVE);
-                event.consume();
-            }
-        });
-
-
-        box1.setOnDragExited(new EventHandler<DragEvent>() {
-            public void handle(DragEvent event) {
-                box1.setFill(Color.BLUE);
-                event.consume();
-            }
-        });
-
-
-        box1.setOnDragDropped(new EventHandler<DragEvent>() {
-            public void handle(DragEvent event) {
-                Dragboard db = event.getDragboard();
-                int handIndex=Integer.parseInt(db.getString());
-                networkHandler.playCard(finalHandPlayable.get(handIndex), coordinates,handCardSides.get(handIndex));
-                Image imageHandCard;
-                String cardId=finalHandPlayable.get(handIndex).getId();
-                if(handCardSides.get(handIndex)==Side.SIDEBACK && cardId.charAt(0)!='s')
-                    cardId=cardId.substring(0,3)+'0';
-                playedCardIndex=handIndex;
-                if(handCardSides.get(handIndex)==Side.SIDEFRONT)
-                    imageHandCard= new Image(getClass().getResourceAsStream("/img/cards/fronts/" + cardId + ".png"));
-                else
-                    imageHandCard= new Image(getClass().getResourceAsStream("/img/cards/backs/" + cardId + ".png"));
-                ImageView newCardImg = new ImageView(imageHandCard);
-                newCardImg.setFitWidth(imageW);
-                newCardImg.setFitHeight(imageH);
-                newCardImg.setTranslateX(posX);
-                newCardImg.setTranslateY(posY);
-                attemptedToPlayCardField=newCardImg;
-                attemptedToPlayCardBox=box1;
-                fieldContainer.getChildren().remove(box1);
-                fieldContainer.getChildren().add(newCardImg);
-                event.setDropCompleted(true);
-                event.consume();
-            }
-        });
-        fieldContainer.getChildren().add(box1);
-    }
     @Override
     public void showInGame() {
         // Displaying the pickable cards and the common objectives
@@ -320,6 +202,28 @@ public class ViewGUIControllerMatch extends ViewGUIController {
             addCardBox(coordinates,finalHandPlayable);
         }
 //        imageContainer.setScaleX(0.5);
+
+        // [Samu] Players container
+        int playerCount = this.state.getPlayers().size();
+        for(Node node: playersContainer.getChildren()) {
+            int currPlayerIdx = GridPane.getRowIndex(node);
+            VBox vBox = (VBox) node;
+            if(currPlayerIdx < playerCount) {
+                PlayerLobby currPlayer = this.state.getPlayers().get(currPlayerIdx);
+                for(Node labelNode: vBox.getChildren()) {
+                    Label label = (Label) labelNode;
+                    switch (label.getId()) {
+                        case "player" -> label.setText(currPlayer.getNickname());
+                        case "online" -> label.setText(this.state.getPlayerState(currPlayer).isConnected() ? "online" : "disconnected");
+                        case "turn" -> label.setText(this.state.getCurrentPlayer().equals(currPlayer) ? "waiting" : "TURN");
+                        case "points" -> label.setText(this.state.getPlayerState(currPlayer).getPoints() + " pts");
+                        default -> throw new RuntimeException("Error while labeling in playerContainer");
+                    }
+                }
+            } else {
+                for(Node labelNode: vBox.getChildren()) ((Label) labelNode).setText("");
+            }
+        }
     }
 
     @Override
@@ -347,6 +251,47 @@ public class ViewGUIControllerMatch extends ViewGUIController {
         }
     }
 
+    @Override
+    public void showPickedCard(PlayerLobby player) {
+        // TODO: Display the card that the player has picked in its hand
+        displayPickablesAndCommonObjs();
+        pickablesContainer.setMouseTransparent(true);
+    }
+
+    @Override
+    public void showNextTurn() {
+        // After a turn has ended, the pickable cards should not be clickable
+
+    }
+
+    // >>> Following methods are ghosts <<<
+    @Override
+    public void showStartupScreen(boolean isSocket, String ip, int port) {}
+    @Override
+    public void setGameId(int gameId) {}
+    @Override
+    public void showPlayedStarter(PlayerLobby player) {}
+    @Override
+    public void showChosenPersonalObjective(PlayerLobby player) {}
+    @Override
+    public void showRooms(List<RoomIF> rooms) {}
+    @Override
+    public void showPlayerJoinedRoom(PlayerLobby player) {}
+    @Override
+    public void showStartGame(GameState state) {}
+
+
+
+    // ----------------------------------------------------------------
+    //      UTILS: PLAYER CONTAINER
+    // ----------------------------------------------------------------
+
+
+
+    // ----------------------------------------------------------------
+    //      UTILS: PICKABLES CONTAINER
+    // ----------------------------------------------------------------
+
     /**
      * Display the pickable cards and the common objectives
      */
@@ -367,16 +312,111 @@ public class ViewGUIControllerMatch extends ViewGUIController {
         displayCard(commonObjectives.get(1).getId(),Side.SIDEFRONT,commonObj2);
     }
 
-    @Override
-    public void showPickedCard(PlayerLobby player) {
-        // TODO: Display the card that the player has picked in its hand
-        displayPickablesAndCommonObjs();
-        pickablesContainer.setMouseTransparent(true);
+    private void makeDraggable(int handPlayableIndex, ImageView imageView){
+        imageView.setId(String.valueOf(handPlayableIndex));
+        imageView.setOnDragDetected(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                Dragboard db = imageView.startDragAndDrop(TransferMode.ANY);
+                ClipboardContent content = new ClipboardContent();
+                content.putString(imageView.getId());
+                db.setContent(content);
+                event.consume();
+            }
+        });
+        imageView.setOnDragDone(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                if (event.getTransferMode() == TransferMode.MOVE) {
+                    //imageView.setImage(null);
+                    attemptedToPlayCardHand=imageView;
+                    handCardsContainer.getChildren().remove(imageView);
+                    for (int i = 0; i < handCardsContainer.getChildren().size(); i++) {
+                        if(handCardsContainer.getChildren().get(i).getOnDragDetected()!=null)
+                            handCardsContainer.getChildren().get(i).setOnDragDetected(null);
+                    }
+                }
+                event.consume();
+            }
+        });
     }
 
-    @Override
-    public void showNextTurn() {
-        // After a turn has ended, the pickable cards should not be clickable
 
+
+    // ----------------------------------------------------------------
+    //      UTILS: FIELD CONTAINER
+    // ----------------------------------------------------------------
+
+    private void addCardBox(Coordinates coordinates, List<CardPlayableIF> finalHandPlayable){
+        Rectangle box1 = new Rectangle(imageW, imageH, Color.BLUE);
+        int posX=(imageW-cornerX)*coordinates.getPosX();
+        int posY=(-imageH+cornerY)*coordinates.getPosY();
+        box1.setTranslateX(posX);
+        box1.setTranslateY(posY);
+
+        box1.setOnDragOver(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                event.acceptTransferModes(TransferMode.MOVE);
+                event.consume();
+            }
+        });
+
+        box1.setOnDragExited(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                box1.setFill(Color.BLUE);
+                event.consume();
+            }
+        });
+
+        box1.setOnDragDropped(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                int handIndex=Integer.parseInt(db.getString());
+                networkHandler.playCard(finalHandPlayable.get(handIndex), coordinates,handCardSides.get(handIndex));
+                Image imageHandCard;
+                String cardId=finalHandPlayable.get(handIndex).getId();
+                if(handCardSides.get(handIndex)==Side.SIDEBACK && cardId.charAt(0)!='s')
+                    cardId=cardId.substring(0,3)+'0';
+                playedCardIndex=handIndex;
+                if(handCardSides.get(handIndex)==Side.SIDEFRONT)
+                    imageHandCard= new Image(getClass().getResourceAsStream("/img/cards/fronts/" + cardId + ".png"));
+                else
+                    imageHandCard= new Image(getClass().getResourceAsStream("/img/cards/backs/" + cardId + ".png"));
+                ImageView newCardImg = new ImageView(imageHandCard);
+                newCardImg.setFitWidth(imageW);
+                newCardImg.setFitHeight(imageH);
+                newCardImg.setTranslateX(posX);
+                newCardImg.setTranslateY(posY);
+                attemptedToPlayCardField=newCardImg;
+                attemptedToPlayCardBox=box1;
+                fieldContainer.getChildren().remove(box1);
+                fieldContainer.getChildren().add(newCardImg);
+                event.setDropCompleted(true);
+                event.consume();
+            }
+        });
+        fieldContainer.getChildren().add(box1);
     }
+
+
+
+    // ----------------------------------------------------------------
+    //      COMMON
+    // ----------------------------------------------------------------
+
+    private void displayCard(String id, Side side, ImageView imageView){
+        Image cardImage;
+        if(side==Side.SIDEBACK && id.charAt(0)!='s')
+            if (id.charAt(0)=='o')
+                id="o000";
+            else
+                id=id.substring(0,3)+'0';
+        if(side==Side.SIDEFRONT)
+            cardImage = new Image(getClass().getResourceAsStream("/img/cards/fronts/" + id + ".png"));
+        else
+            cardImage = new Image(getClass().getResourceAsStream("/img/cards/backs/" + id + ".png"));
+        imageView.setImage(cardImage);
+        imageView.setFitHeight(imageH);
+        imageView.setFitWidth(imageW);
+    }
+
+
 }
